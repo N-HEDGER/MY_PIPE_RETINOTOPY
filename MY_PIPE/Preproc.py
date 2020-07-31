ec47163f987a3da9621994f81a10ba6709aca088
N-HEDGER <nhedger1@gmail.com>
2020-07-31 15:51:09 +0100

    nb

--- MY_PIPE/Preproc.py
+++ MY_PIPE/Preproc.py
@@ -58,6 +58,12 @@ def savgol_filter(data, polyorder, deriv, window_length,tr,fmt):
 
 
 
+
+
+
+
+
+
 def perform_savgol (dlist, polyorder, deriv, window_length,tr,procs,fmt):
     filt_dat=Parallel(n_jobs=procs,verbose=9)(delayed(savgol_filter)(data,polyorder, deriv, window_length,tr,fmt)  for data in dlist)
     return filt_dat
@@ -65,6 +71,133 @@ def perform_savgol (dlist, polyorder, deriv, window_length,tr,procs,fmt):
 
 
 
+def savgol_filter_1D(data, polyorder, deriv, window_length,TR):
+
+
+    from scipy.signal import savgol_filter
+
+    window = np.int(window_length / TR)
+
+    # Window must be odd
+    if window % 2 == 0:
+        window += 1
+
+    data_filt = savgol_filter(data, window_length=window, polyorder=polyorder,
+                              deriv=deriv, mode='nearest')
+
+    data_filtered = data - data_filt + data_filt.mean()
+
+    return data_filtered
+
+
+
+def denoise(data,nuissance,varr,n_components, polyorder, deriv, window_length,TR,fmt):
+    df = pd.read_csv(nuissance, sep='\t', header=0,index_col=None)
+    # get the wanted variables and do stuff with it
+    nuissances = []
+    for var in varr:
+        # get wanted nuissance variables
+        ndat = np.array(df[var])
+        # fill in nans
+        ndat[ndat=='n/a'] = np.nan
+        # cast to float
+        ndat = ndat.astype(fmt)
+            # median fill nan values (i.e. first value )
+        ndat[np.isnan(ndat)] = np.nanmedian(ndat)
+            # temporally filter 
+        filtered_data = savgol_filter_1D(ndat, polyorder, deriv, window_length,TR)
+            # z-score (so that explained variance ratios is interpretable)
+        filtered_data_z = (filtered_data - np.mean(filtered_data)) / np.std(filtered_data)
+            # and append
+        nuissances.append(filtered_data_z)
+
+    nuissances = np.array(nuissances)    
+
+        # now do pca and grab first 5:
+    pca = PCA(n_components=n_components)  
+    pcas = pca.fit_transform(nuissances.T)
+
+    datashape = data.shape
+
+        # do nuissance regression
+    dm = np.hstack([np.ones((pcas.shape[0],1)),pcas]) # add intercept
+    model = OLSModel(dm)
+    fit = model.fit(data.reshape(-1,datashape[-1]).T)
+    resid = fit.resid.T.reshape(datashape)
+    resid += np.mean(data,axis=-1)[:,np.newaxis] # re-add the signal offset which was regressed out by the intercept
+    return resid, fit.theta,fit.r_square
+
+
+def perform_denoising (dlist,nlist,varr,n_components, polyorder, deriv, window_length,TR,procs,fmt):
+
+    if len(nlist) != len(dlist):
+
+        raise Exception("The number of datafiles and nuissance files do not match")
+
+    denoised_dat=Parallel(n_jobs=procs,verbose=9)(delayed(denoise)(dlist[i],nlist[i],varr,n_components, polyorder, deriv, window_length,TR,fmt)  for i in range(len(dlist)))
+    return denoised_dat
+
+import nibabel
+import os
+
+
+
+def export_noisefit(denoised_dat,export,sub,path,hem):
+    R2s=[]
+    for dn in denoised_dat:
+        R2s.append(np.nan_to_num(dn[2],0))
+        bs.append(np.nan_to_num(dn[1],0))  
+
+
+
+
+    bs=np.mean(bs,axis=0)    
+    R2s=np.squeeze(np.mean(R2s,axis=0))
+
+    if export:
+        nibabel.freesurfer.io.write_morph_data(os.path.join(path,sub+'_'+hem+'_'+'noise_R2_curv'),R2s)
+        minimal_plot(sub,R2s,True,full_fig,True,os.path.join(path,sub+'_'+hem+'_'+'noise_R2.png'))
+
+    for i in range(t.shape[0]):
+
+        nibabel.freesurfer.io.write_morph_data(os.path.join(path,sub+'_'+hem+'_'+'pc'+str(i)+'noise_beta_curv'),s[i,:])
+        minimal_plot(sub,bs[i,:],True,full_fig,True,os.path.join(path,sub+'_'+hem+'_'+'pc'+str(i)+'noise_beta.png'))
+
+
+
+    return R2s, bs
+
+
+
+def compute_tsnr(data,fmt):
+    tsnr = np.mean(data,axis=-1)/np.std(data,axis=-1)
+    tsnr[np.isnan(tsnr)] = 0
+    tsnr[np.isinf(tsnr)] = 0
+    tsnr += np.abs(np.min(tsnr))+0.01
+    tsnr=np.squeeze(tsnr)
+    return tsnr.astype(fmt)
+
+
+def perform_tsnring(dlist,procs,fmt):
+    tsnr_dat=Parallel(n_jobs=procs,verbose=9)(delayed(compute_tsnr)(data,fmt)  for data in dlist)
+    return tsnr_dat
+
+
+
+def export_tsnr(tsnr_dat,export,sub,path,hem):
+    tsnr=[]
+    for ts in tsnr_dat:
+        tsnr.append(ts)
+
+
+    tsnr=np.mean(tsnr,axis=0)    
+
+    if export:
+        nibabel.freesurfer.io.write_morph_data(os.path.join(path,sub+'_'+hem+'_'+'tsnr_curv'),tsnr)
+        minimal_plot(sub,R2s,True,full_fig,True,os.path.join(path,sub+'_'+hem+'_'+'tsnr.png'))
+
+
+    return tsnr
 
 
 
--- MY_PIPE/Utils.py
+++ MY_PIPE/Utils.py
@@ -1,6 +1,7 @@
 
 
 import json
+import os
 
 
 def init(jpath):
@@ -9,7 +10,7 @@ def init(jpath):
     return data
 
 
-def findgiftis(FOLDER, ext):
+def findtype(FOLDER, ext):
 
     # Function goal: return nifti files within a directory
 
@@ -32,6 +33,18 @@ def findgiftis(FOLDER, ext):
     return(sorted(giftilist))
 
 
+def dill_save(obj, directory, fname):
+
+    with open(os.path.join(directory, fname), 'wb') as output:
+        dill.dump(obj, output)
+
+
+def load(filename):
+    input_file = open(filename, 'rb')
+    obj = dill.load(input_file)
+    return obj
+
+
 def make_gifti_list(base_folder, sessionstring, space):
 
     def fprintf(stream, format_spec, *args):
@@ -46,15 +59,87 @@ def make_gifti_list(base_folder, sessionstring, space):
     fprintf(sys.stdout, "Found %d sessions \n", len(sessionlist))
 
     gifts = []
+
     for sess in sorted(sessionlist):
-    	gifts.append(findgiftis(os.path.join(sess, 'func'), '.gii'))
+        gifts.append(findtype(os.path.join(sess, 'func'), '.gii'))
 
-	flatgifts = []
-	for sublist in gifts:
-		for item in sublist:
-                flatgifts.append(item)
+    flatgifts = []
 
-	leftgifts = [gift for gift in flatgifts if space + "_hemi-L" in(gift)]
-	rightgifts = [gift for gift in flatgifts if space + "_hemi-R" in(gift)]
+    for sublist in gifts:
+        for item in sublist:
+            flatgifts.append(item)
 
-	return leftgifts, rightgifts, gifts, flatgifts
+    leftgifts = [gift for gift in flatgifts if space + "_hemi-L" in(gift)]
+    rightgifts = [gift for gift in flatgifts if space + "_hemi-R" in(gift)]
+
+    return leftgifts, rightgifts, gifts, flatgifts
+
+
+
+
+def make_tsv_list(base_folder, sessionstring, space):
+
+    def fprintf(stream, format_spec, *args):
+        stream.write(format_spec % args)
+
+    import os
+    import sys
+    sessionlist = list()
+    for session in os.listdir(base_folder):
+        if not session.startswith('.') and session.startswith(tuple(sessionstring)):
+            sessionlist.append(os.path.join(base_folder, session))
+    fprintf(sys.stdout, "Found %d sessions \n", len(sessionlist))
+
+    gifts = []
+
+    for sess in sorted(sessionlist):
+        gifts.append(findtype(os.path.join(sess, 'func'), '.tsv'))
+
+    flatgifts = []
+
+    for sublist in gifts:
+        for item in sublist:
+            flatgifts.append(item)
+
+
+    return gifts, flatgifts
+
+
+from nilearn import surface
+
+from joblib import Parallel, delayed
+
+
+def import_surf_astype(fn,fmt):
+    impt=surface.load_surf_data(fn).astype(fmt)
+    return impt
+
+
+
+def import_gifts(flist, procs,fmt):
+    gifts = Parallel(n_jobs=procs, verbose=9)(delayed(import_surf_astype)(fn,fmt) for fn in flist)
+    return gifts
+
+
+
+
+
+def make_webviewer_dict(sub,data,labels):
+
+
+    mydict= {}
+
+    for d in range(len(data)):
+        data[d]=data[d].astype('float64')
+        vdat = cortex.Vertex(data[d], subject=sub,vmin=np.nanmin(data[d]),vmax=np.nanmax(data[d]),cmap='plasma')
+        mydict[labels[d]]=vdat
+
+    return mydict
+
+
+
+
+
+
+
+
